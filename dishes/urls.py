8c0cdbd6f12a1e11a4e2324f4cf7eb256fef8d4e
Oswaldinho <oswalfut_96@hotmail.com>
2018-04-13 16:42:14 -0500

    loool

--- dishes/urls.py
+++ dishes/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import DishViewSet, RestaurantViewSet
+from accounts.views import UserViewSet
 
 app_name="dishes"
 
 router = routers.DefaultRouter()
 router.register('dishes', DishViewSet)
 router.register('restaurants', RestaurantViewSet)
+router.register('users', UserViewSet)
+
 
 urlpatterns=[
 	path('', include(router.urls)),
--- dishes/views.py
+++ dishes/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Dish, Restaurant
 from .serializers import DishSerializer, RestaurantSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class DishViewSet(viewsets.ModelViewSet):
 	queryset = Dish.objects.all()
 	serializer_class = DishSerializer
+	permission_classes = [IsAuthenticated]
 
 class RestaurantViewSet(viewsets.ModelViewSet):
 	queryset = Restaurant.objects.all()
 	serializer_class = RestaurantSerializer
+	permission_classes = [IsAuthenticated]
